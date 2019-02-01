381021fae38da87d697b819dccf49b927d427983
recredit <46069475+recredit@users.noreply.github.com>
2019-02-01 13:39:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`انا مدير كبيير`,"http://twitch.tv/Dream")
+client.user.setGame(`انا مدير كبيير`,"http://twitch.tv/YouTubeee")
 client.user.setStatus("dnd")
 });
 
